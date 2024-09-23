4bd693265a8afc944506cf6279497e7a981588ec
Youngmin Jo <54759844+designeryoungmin@users.noreply.github.com>
2024-09-24 00:06:39 +0900

    Refactoring

--- boan_news_scraping.py
+++ boan_news_scraping.py
@@ -1,3 +1,4 @@
+import os
 import requests
 import pandas as pd
 from bs4 import BeautifulSoup
@@ -6,13 +7,20 @@ from datetime import datetime
 
 data = []
 
+now = datetime.now()
+formatted_date = now.strftime("%Y_%m_%d")
+
+directory = "./excel_files"
+excel_file = f"{directory}/boan_news_{formatted_date}.xlsx"
+
+if not os.path.exists(directory):
+        os.makedirs(directory)
+
 headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36"
     }
 
-# 제목에 오늘 날짜 추가용
-now = datetime.now()
-formatted_date = now.strftime("%Y_%m_%d")
+
 
 for i in range(1, 10 + 1): # 1 ~ 10 페이지까지
     # 보안뉴스 취약점 경고 및 보안 업데이트 페이지
@@ -37,6 +45,6 @@ for i in range(1, 10 + 1): # 1 ~ 10 페이지까지
 table_headers = ["제목", "본문", "링크"] # 헤더 부분
 df = pd.DataFrame(data, columns=table_headers) # 데이터 프레임 생성
 df.index = df.index + 1 # 인덱스 번호 1부터
-df.to_excel(f"boan_news_{formatted_date}.xlsx", index_label='No') # xlsx 형식으로 저장
+df.to_excel(excel_file, index_label='No') # xlsx 형식으로 저장
 
 print("[+] 작업이 완료되었습니다.")
